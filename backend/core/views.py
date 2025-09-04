bf9c30e5fb543a87c32f26e7d255db176cc1131d
lEduardo Reis <beefarias15@gmail.com>
2025-09-03 23:42:44 -0300

    feat: Implementação API REST para CRUD de Produtos e Categorias

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Categoria, Produto
+from .serializers import CategoriaSerializer, ProdutoSerializer
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    """
+    Endpoint da API que permite que as categorias sejam visualizadas ou editadas.
+    """
+    queryset = Categoria.objects.all().order_by('nome')
+    serializer_class = CategoriaSerializer
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    """
+    Endpoint da API que permite que os produtos sejam visualizados ou editados.
+    """
+    queryset = Produto.objects.all().order_by('data_validade')
+    serializer_class = ProdutoSerializer
