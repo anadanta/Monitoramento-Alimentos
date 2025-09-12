80ad3e69aaf85ae0e3473a185fb78dc88413722b
Maykon Carvalho <carvalhomaykon566@gmail.com>
2025-09-12 11:18:45 -0300

    editando banco de dados de produtos

--- backend/core/models.py
+++ backend/core/models.py
@@ -13,16 +13,31 @@ class Categoria(models.Model):
         verbose_name = "Categoria"
         verbose_name_plural = "Categorias"
 
+class StatusProduto(models.TextChoices):
+    NORMAL = "normal", "Normal"
+    ATENCAO = "atencao", "Atenção"
+    URGENTE = "urgente", "Urgente"
+    VENCIDO = "vencido", "Vencido"
+    VENDIDO = "vendido", "Vendido"
+
+class UnidadeMedida(models.TextChoices):
+    KG = "quilograma", "Quilograma"
+    UN = "unidade", "Unidade"
+    L = "litro", "Litro"
+
 class Produto(models.Model):
     usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
     nome = models.CharField(max_length=200, help_text="Nome do produto")
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True, related_name='produtos')
     data_validade = models.DateField(help_text="Data de vencimento do produto")
+    unidade_medida = models.CharField(max_length=20, choices=UnidadeMedida.choices, default=UnidadeMedida.UN, null=True, blank=True)
     quantidade_disponivel = models.PositiveIntegerField(help_text="Quantidade em estoque")
+    status = models.CharField(max_length=20, choices=StatusProduto.choices, null=True, blank=True)
+    codigo = models.CharField(max_length=20, unique=True, null=True, blank=True)
 
     def __str__(self):
         return f"{self.nome} (Validade: {self.data_validade.strftime('%d/%m/%Y')})"
-
+    
     class Meta:
         verbose_name = "Produto"
         verbose_name_plural = "Produtos"
