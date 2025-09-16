b51bd448aa60cc4a8b901f031747efb731325a9f
carvalhomaykon <carvalhomaykon566@gmail.com>
2025-09-16 18:47:44 -0300

    feat: UI: Componentes de filtro

--- backend/produtos/views.py
+++ backend/produtos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from produtos.forms import ProdutoForm
-from core.models import Produto
+from core.models import Produto, StatusProduto, Categoria
 from usuarios.models import Usuarios
 import datetime
 from django.contrib import messages
@@ -11,14 +11,56 @@ def lista_produtos(request):
 
     usuario = Usuarios.objects.get(id=request.session['usuario_id'])
     produtos = Produto.objects.filter(usuario=usuario)
+    categorias = Categoria.objects.all()
 
-    form = ProdutoForm()
+    # -- Filtro que pesquisa por nome do produto --
+    query = request.GET.get('q')
+    if query:
+        produtos = produtos.filter(nome__icontains=query)
+
+    # -- Ordenação por data de validade --
+    sort_by = request.GET.get('sort')
+    if sort_by == 'data_asc':
+        produtos = produtos.order_by("data_validade")
+    elif sort_by == 'data_desc':
+        produtos = produtos.order_by("-data_validade")
+
+    # -- Filtro por status --
+    status = request.GET.get('status')
+    if status and status in dict(StatusProduto.choices).keys():
+        produtos = produtos.filter(status=status)
+
+    # -- Filtro por categoria --
+    categoria_id = request.GET.get('categoria')
+    if categoria_id:
+        produtos = produtos.filter(categoria_id=categoria_id)
+
+    # -- Filtro pelo intervalo de data de validade do produto --
+    data_inicio = request.GET.get('data_inicio')
+    data_fim = request.GET.get('data_fim')
+    if data_inicio and data_fim:
+        produtos = produtos.filter(data_validade__range=[data_inicio, data_fim])
+    elif data_inicio:
+        produtos = produtos.filter(data_validade__gte=data_inicio)
+    elif data_fim:
+        produtos = produtos.filter(data_validade__lte=data_fim)
+
+    # -- Ordenação por data de quantidade disponível (Estoque) --
+    sort_by_quantidade = request.GET.get('sort_validade')
+    if sort_by_quantidade == 'quantidade_asc':
+        produtos = produtos.order_by("quantidade_disponivel")
+    elif sort_by_quantidade == 'quantidade_desc':
+        produtos = produtos.order_by("-quantidade_disponivel")
+
+    form = ProdutoForm()    
 
     context = {
         'title': 'Produtos',
         'produtos': produtos,
         'title': 'Listagem de Produtos',
-        'form': form
+        'form': form,
+        "status_choices": StatusProduto.choices,
+        'categorias': categorias
     }
 
     if request.method == 'POST':
